bbad2543b8c96d5f067ec99c8d0147a60a32a606
Lucas Yan <lucas@peer.com>
2016-03-28 15:35:06 -0700

    Fixed emails

--- components/clubItem.jsx
+++ components/clubItem.jsx
@@ -41,13 +41,31 @@ class ClubItem extends React.Component {
                 <div className="group">
                     <div className="field-label">Email</div>
                     <div className="field">
-                        <a href={`mailto:${this.props.club.email}`}>
-                            {this.props.club.email}
-                        </a>
+                        {this.renderEmails(this.props.club.email)}
                     </div>
                 </div>
             </div>
         );
+    }
+
+    renderEmails(emailString) {
+        if (!emailString) {
+            return;
+        }
+        let emails = emailString.split(',');
+        return (
+            emails.map(function(email, index) {
+                email = email.trim();
+                return (
+                    <span key={index}>
+                        <a href={`mailto:${email}`}>
+                            {email}
+                         </a>
+                        {index < emails.length - 1 ? " " : ""}
+                    </span>
+                )
+            })
+        )
     }
 }
 
