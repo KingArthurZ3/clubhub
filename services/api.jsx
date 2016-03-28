4cba9007b07e4719777c945b43532a93245b34ef
Lucas Yan <lucas@peer.com>
2016-03-28 15:47:24 -0700

    Minor fixes

--- services/api.jsx
+++ services/api.jsx
@@ -9,27 +9,26 @@ export default {
             }, {
                 "api_key": "br_28029_d069ee1984a468c879dc72cf038c089798b5ba4d"
             }, function (res) {
-                var array = jQuery.map(res.params, function (value, index) {
+                var array = jQuery.map(res.params, function (value) {
                     return [value];
                 });
                 var rows = [];
                 for (var i = 0; i < array[0].length; i++) {
-                    var test = jQuery.map(array[0][i], function (value, index) {
+                    var test = jQuery.map(array[0][i], function (value) {
                         return [value];
                     });
                     rows.push(test.slice(0, -1));
                 }
-                var clubs = rows.map(function (club) {
-                    var club = {
-                        name: club[0],
-                        advisor: club[1],
-                        room: club[2],
-                        time: club[3],
-                        purpose: club[7],
-                        president: club[4],
-                        email: club[5]
+                var clubs = rows.map(function (row) {
+                    return {
+                        name: row[0],
+                        advisor: row[1],
+                        room: row[2],
+                        time: row[3],
+                        purpose: row[7],
+                        president: row[4],
+                        email: row[5]
                     };
-                    return club;
                 });
                 callback(clubs);
             }
