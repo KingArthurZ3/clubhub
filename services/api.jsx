61696709fb31aaf7cafd1bd8e5004b3e16b33c39
Lucas Yan <lucas@peer.com>
2017-04-11 11:03:05 -0700

    Fix API

--- services/api.jsx
+++ services/api.jsx
@@ -1,37 +1,34 @@
-import Blockspring from './blockspring';
 import jQuery      from 'jquery';
+
+// JSON feed of the spreadsheet.
+const sheetsUrl =
+    "https://spreadsheets.google.com/feeds/list/12lZPb-BKuDsEVWirwm8puMrvmACoaNYJyS5A_vjnVak/od6/public/values?alt=json";
+
+// console.log row to figure out the keys of these columns.
+// then do row[<keyname>]["$t"] to access their string values.
+const nameKey = "gsx$clubname";
+const advisorKey = "gsx$advisorname";
+const roomKey = "gsx$wheredoesyourclubmeet";
+const timeKey = "gsx$whendoesyourclubmeet";
+const purposeKey = "gsx$descriptionofclub";
+const presidentKey = "gsx$nameofclubpresidents";
+const emailKey = "gsx$contactemailoftheclub";
 
 export default {
     getClubs: (callback) => {
-        Blockspring.runParsed("query-public-google-spreadsheet", {
-                "query": "SELECT *",
-                "url": "https://docs.google.com/spreadsheets/d/12lZPb-BKuDsEVWirwm8puMrvmACoaNYJyS5A_vjnVak/edit?usp=sharing"
-            }, {
-                "api_key": "AIzaSyDkut1nm95ecS9ObPlWDX-eKdzQX229YQI"
-            }, function (res) {
-                var array = jQuery.map(res.params, function (value) {
-                    return [value];
-                });
-                var rows = [];
-                for (var i = 0; i < array[0].length; i++) {
-                    var test = jQuery.map(array[0][i], function (value) {
-                        return [value];
-                    });
-                    rows.push(test.slice(0, -1));
-                }
-                var clubs = rows.map(function (row) {
-                    return {
-                        name: row[0],
-                        advisor: row[1],
-                        room: row[2],
-                        time: row[3],
-                        purpose: row[7],
-                        president: row[4],
-                        email: row[5]
-                    };
-                });
-                callback(clubs);
-            }
-        );
+        jQuery.getJSON(sheetsUrl, data => {
+            var clubs = data.feed.entry.map(row => {
+                return {
+                    name: row[nameKey]["$t"],
+                    advisor: row[advisorKey]["$t"],
+                    room: row[roomKey]["$t"],
+                    time: row[timeKey]["$t"],
+                    purpose: row[purposeKey]["$t"],
+                    president: row[presidentKey]["$t"],
+                    email: row[emailKey]["$t"]
+                };
+            });
+            callback(clubs);
+        });
     }
 }
